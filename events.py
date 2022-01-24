df4a2fa073e94635e44d24d096616f941776415b
Steven <stevengf14@gmail.com>
2022-01-24 12:55:52 -0500

    modifying click event

--- events.py
+++ events.py
@@ -7,17 +7,15 @@ class PrincipalWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle('Signals and Slots')
         # Button
-        button = QPushButton('Click here')
-        button.setCheckable(True)
-        button.clicked.connect(self.check_event)
-        button.clicked.connect(self.click_event)
-        self.setCentralWidget(button)
+        self.button = QPushButton('Click here')
+        self.button.clicked.connect(self.click_event)
+        self.setCentralWidget(self.button)
 
     def click_event(self):
-        print('Clicked')
-
-    def check_event(self, check):
-        print(f'Checked? {check}')
+        self.button.setText('New Text')
+        self.setEnabled(False)
+        self.setWindowTitle('New App Title')
+        print('Click event')
 
 if __name__ == '__main__':
     app = QApplication([])
