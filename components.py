114a79c2206b3b3b32d187b15787b66e98bab115
Steven <stevengf14@gmail.com>
2022-01-25 10:58:12 -0500

    Load Images

--- components.py
+++ components.py
@@ -1,18 +1,15 @@
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QMainWindow, QLabel, QApplication
 
 class Components(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Components')
+        self.setFixedSize(500, 600)
         tag = QLabel('Hi')
-        tag.setText('Welcome')
-        font = tag.font()
-        # Default value = 12
-        font.setPointSize(25)
-        tag.setFont(font)
-        # tag.setAlignment(Qt.AlignCenter)
-        tag.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        tag.setPixmap(QPixmap('spidey.jpg'))
+        # tag.setScaledContents(True)
         self.setCentralWidget(tag)
 
 if __name__ == '__main__':
