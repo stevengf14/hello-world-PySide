eee088d0279131c0e34b57b9a39625fd598ccdf8
Steven <stevengf14@gmail.com>
2022-01-26 12:34:20 -0500

    Component QSlider

--- components.py
+++ components.py
@@ -1,26 +1,31 @@
-from PySide6.QtWidgets import QMainWindow, QApplication, QSpinBox, QDoubleSpinBox
+from PySide6.QtWidgets import QMainWindow, QApplication, QSpinBox, QDoubleSpinBox, QSlider
 
 
 class Components(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Components')
-        number = QDoubleSpinBox()
-        # number.setMinimum(-5)
-        # number.setMaximum(5)
-        number.setRange(-25.5, 25.5)
-        number.setPrefix('$')
-        number.setSuffix(' or USD')
-        number.setSingleStep(3.5)
-        number.valueChanged.connect(self.change_value)
-        number.textChanged.connect(self.change_text)
-        self.setCentralWidget(number)
+        component = QSlider()
+        # component = QSlider(Qt.Horizontal)
+        component.setMinimum(-25)
+        component.setMaximum(25)
+        component.valueChanged.connect(self.change_value)
+        component.sliderMoved.connect(self.slider_moved)
+        component.sliderPressed.connect(self.slider_pressed)
+        component.sliderReleased.connect(self.slider_released)
+        self.setCentralWidget(component)
 
     def change_value(self, new_value):
         print(f'New value: {new_value}')
 
-    def change_text(self, new_text):
-        print(f'New text: {new_text}')
+    def slider_moved(self, new_position):
+        print(f'New position: {new_position}')
+
+    def slider_pressed(self):
+        print('Slider pressed')
+
+    def slider_released(self):
+        print('Slider released')
 
 if __name__ == '__main__':
     app = QApplication([])
