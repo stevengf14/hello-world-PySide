fbdbfca247917dae2d4be20ae89872a5886e237c
Steven <stevengf14@gmail.com>
2022-01-26 12:21:23 -0500

    Component QDoubleSpinBox

--- components.py
+++ components.py
@@ -1,17 +1,17 @@
-from PySide6.QtWidgets import QMainWindow, QApplication, QSpinBox
+from PySide6.QtWidgets import QMainWindow, QApplication, QSpinBox, QDoubleSpinBox
 
 
 class Components(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Components')
-        number = QSpinBox()
+        number = QDoubleSpinBox()
         # number.setMinimum(-5)
         # number.setMaximum(5)
-        number.setRange(-25, 25)
+        number.setRange(-25.5, 25.5)
         number.setPrefix('$')
         number.setSuffix(' or USD')
-        number.setSingleStep(3)
+        number.setSingleStep(3.5)
         number.valueChanged.connect(self.change_value)
         number.textChanged.connect(self.change_text)
         self.setCentralWidget(number)
